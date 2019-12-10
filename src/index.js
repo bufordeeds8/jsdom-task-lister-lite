b69ea2aa7bf3ba13d481e3586caf8b6bc0da5c1c
Buford Eeds <bufordeeds8@gmail.com>
2019-12-10 16:57:04 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+
+  const taskForm = document.querySelector('#create-task-form')
+  const taskList = document.querySelector('#tasks')
+
+  taskForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const newTask = document.querySelector('#new-task-description').value;
+  // const taskItem = document.createElement('li');
+  // taskItem.innerText = newTask;
+  // taskList.appendChild(taskItem);
+
+  taskList.innerHTML += `
+    <li>${newTask}
+      <button class="delete-task">X</button>
+    </li>
+    `
+    const button = document.getElementsByClassName('#delete')
+
+
+
+
+
+
+  });
+ 
+
 });
